9d110fe092b85d88525721650ae43ab9999330d1
Erwin Erdem <41726224+erwinerdem@users.noreply.github.com>
2021-04-15 22:07:04 +0200

    Update Descartes_Human_Cerebrum.R

--- Descartes_Human_Cerebrum.R
+++ Descartes_Human_Cerebrum.R
@@ -9,10 +9,10 @@ for (i in sample_range){
     print(s_out)
     s_data = paste(s_out, '/data', sep="")
     s_colnames = paste(s_out, "/colnames", sep="")
-	pos <- as.numeric(readLines(paste("positions", i, ".txt", sep="")))
-	h5createGroup("Cerebrum_gene_count.h5", s_out)
-	sampled_data <- data[,pos]
-	full_sampled_data <- as.matrix(sampled_data)
-	h5write(full_sampled_data, "Cerebrum_gene_count.h5", s_data)
-	h5write(colnames(full_sampled_data), "Cerebrum_gene_count.h5", s_colnames)
+    pos <- as.numeric(readLines(paste("positions", i, ".txt", sep="")))
+    h5createGroup("Cerebrum_gene_count.h5", s_out)
+    sampled_data <- data[,pos]
+    full_sampled_data <- as.matrix(sampled_data)
+    h5write(full_sampled_data, "Cerebrum_gene_count.h5", s_data)
+    h5write(colnames(full_sampled_data), "Cerebrum_gene_count.h5", s_colnames)
 }
